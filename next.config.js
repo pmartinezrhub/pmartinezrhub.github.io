7b5ff9ebf149bb3013cf7c55de655dfc08756b5f
pmartinezrhub <106752230+pmartinezrhub@users.noreply.github.com>
2025-08-15 20:13:22 +0200

    Update next.config.js
    fix wrong sintax

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const nextConfig = {
   /**added cause error=>  `next export` has been removed in favor of 'output: export' in next.config.js.
   You should retire this line if you use this out of github deploymen */
- output:'export',
- images.unoptimized = true
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
